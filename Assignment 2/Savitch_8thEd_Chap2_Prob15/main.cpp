411b2f0192cc3ead292a8d110756fbeec2f8b45b
Jonathan Tarris <jonathandtarris@gmail.com>
2014-01-19 06:40:29 -0800

    signed off by Slyonnic

--- Assignment 2/Savitch_8thEd_Chap2_Prob15/main.cpp
+++ Assignment 2/Savitch_8thEd_Chap2_Prob15/main.cpp
@@ -16,16 +16,16 @@ using namespace std;
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare Vars
-    float vel, stemp, etemp;
+    float vel, sTemp, eTemp;
     
     //Prompt for user input
     cout<<"What is starting temperature in Celsius?"<<endl;
-    cin>>stemp;
+    cin>>sTemp;
     cout<<"What is ending temperature in Celsius?"<<endl;
-    cin>>etemp;
+    cin>>eTemp;
     
     //Loop and display results
-    for(int i = stemp; i <= etemp; i++){
+    for(int i = sTemp; i <= eTemp; i++){
         vel = 331.3 + (0.61*i);
         cout<<"At "<<i<<" degrees Celsius the velocity of sound is "<<vel<<" m/s"<<endl;
     }
